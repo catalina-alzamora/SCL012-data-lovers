394a13d2d048c862ff9088d733fbebf66bbd10f8
catalina-alzamora <alzamoracatalina@gmail.com>
2019-11-29 13:51:31 -0300

    probando actualización

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
 
-import { example } from './data.js';
+import  POKEMON  from './data/pokemon/pokemon.js';
+ 
+//import { example } from './data.js';
 
-console.log(example);
+console.log(POKEMON [0]);
 
 /*
  * console.log(POKEMON);
