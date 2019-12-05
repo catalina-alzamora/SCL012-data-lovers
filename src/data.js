d4016c5a171622ef54355b583eb77a560a135742
catalina-alzamora <alzamoracatalina@gmail.com>
2019-12-05 10:10:09 -0300

    probando javascript

--- src/data.js
+++ src/data.js
@@ -5,3 +5,13 @@
 export const example = () => {
   return 'example';
 };
+
+
+/*función de filterData(data, condition): esta función filter o filtrar recibiría la data, 
+y nos retornaría aquellos datos que sí cumplan con la condición*/
+export const filteredPokemon = () => { POKEMON.filter(d => d.type.includes= "Grass");
+return filteredPokemon;
+}
+/*sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data,
+nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere
+ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente*/
--- src/main.js
+++ src/main.js
@@ -1,12 +1,47 @@
 
 import  POKEMON  from './data/pokemon/pokemon.js';
- 
-//import { example } from './data.js';
 
+//probando si sale el objeto bulbasaur
 console.log(POKEMON [0]);
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
+//viendo keys de bulbasaur
+console.log(Object.keys(POKEMON [0]));
+/*Keys de cada pokemon:
+"id", "num", "name", "img", "type", "height", "weight", "candy", "candy_count", "egg", "spawn_chance",
+ "avg_spawns", "spawn_time", "multipliers", "weaknesses", "next_evolution"*/
+
+//viendo valor de key-tipo de bulbasaur
+console.log(POKEMON[0].type);
+
+/*Probando si recorre
+for (let i = 0; i <= POKEMON.length; i++) {
+    console.log(POKEMON[i].type);
+}
 */
+//probando hacer arreglo con pokemons tipo planta
+function filterByGrass(POKEMON){
+let grassPokemon = [];
+for (let i = 0; i <= POKEMON.length; i++) {
+    if ((POKEMON[i].type).includes("Grass")) {
+     grassPokemon.push(POKEMON[i]);  
+     console.log(grassPokemon)
+    }}    
+   return grassPokemon
+}
+
+
+
+    //función de cambio de pantalla con el botón de ingresar
+document.getElementById('playBtn').addEventListener('click', () => {
+    document.getElementById('screenTwo').style.visibility='visible';
+    document.getElementById('screenTwo').style.display='block';
+    document.getElementById('screenOne').style.visibility='hidden';
+    document.getElementById('screenOne').style.display='none';
+    
+    //Declarando variable para imprimir pokemon
+    let print= document.getElementById ("pokemonList");
+    //imprimiendo nombre de primer pokemon
+    print.innerHTML= POKEMON[0].name;
+
+
+})
