16e1ea13ac98f16dd41a627bb0e58ff8f97f82b2
JocelynJofreAntoine <joce.jofreantoine@gmail.com>
2019-12-13 09:22:23 -0300

    casi completo

--- README.md
+++ README.md
@@ -9,12 +9,13 @@
  * Sketch
  * Prototipo
  * Test de Usabilidad
+ 
 
 ***
 
 ## Introducción
 
-Pokémon GO es una aplicación que permite a los jugadores de esta poder acceder a distinta informción sobre sus Pokémon, todo lo que necesiten saber pueden encontrarla en en esta app. 
+Pokémon GO es una aplicación que permite a los jugadores de esta poder acceder a distinta información sobre sus Pokémon, todo lo que necesiten saber pueden encontrarla en en esta app. 
 
 ***
 
@@ -60,77 +61,59 @@ Las historias de usuario están basadas en las necesidades que tiene el usuario 
   ### Criterios de aceptación:
 
     Usuario puede ingresar a la aplicación.
-    Usuario puede filtrar la lista por tipo de pokemon.
-    Usuario puede hacer click en un pokemon y ver sus características.
-
-
-### Historia de usuario n°2
-
-     YO: jugador de pokemon Go 
-     QUIERO: Saber cuántos caramelos necesito 
-     PARA: poder evolucionar mi pokemon. 
-
-  ### Criterios de aceptación:
-
-    -
-    -
-    -
-
-
-### Historia de usuario n°3
-
-     YO: jugador de pokemon Go 
-     QUIERO: saber las evoluciones 
-     PARA: Saber en qué evoluciona mi pokemon.
-
-  ### Criterios de aceptación:
-
-    -
-    -
-    -
-
+    Usuario puede filtrar la lista por tipo de Pokémon y ver sus características.
+   
 
 *** 
 ## Sketch 
 
 * Para Celular
 
-![Sketch] (https://github.com/JocelynJofreAntoine/SCL012-data-lovers/blob/master/src/img/celu.jpg?raw=true)
+ ![ sketch](/src/img/celu.jpg)
 
 * Para Web 
 
-![Sketch] (https://github.com/JocelynJofreAntoine/SCL012-data-lovers/blob/master/src/img/web.jpg?raw=true)
+![ sketch](/src/img/web.jpg)
 
 
-## Prototipos
+## Prototipo
 
 Prototipo baja calidad Celular 
-![Prototipo] (https://raw.githubusercontent.com/JocelynJofreAntoine/SCL012-data-lovers/master/src/img/ProtBC.PNG)
+![ prototipo](/src/img/ProtBC.png)
 
-![Prototipo] (https://github.com/JocelynJofreAntoine/SCL012-data-lovers/blob/master/src/img/ProtAC.PNG?raw=true)
+Protitipo alta calidad Celular
 
-![Prototipo] (https://github.com/JocelynJofreAntoine/SCL012-data-lovers/blob/master/src/img/ProtACWeb.PNG?raw=true)
+![ prototipo](/src/img/ProtAC.png)
 
-Prototipo de alta calidad Celular 
+Protitipo alta calidad Web
+
+![ prototipo](/src/img/ProtACWeb.png)
+
+
+Prototipo de alta calidad Celular, puedes interactuar en el siguiente link: 
 https://www.figma.com/proto/M9bUJm5R6pcHAjHthwYKYj/datago?node-id=11%3A237&scaling=scale-down
 
-Prototipo de alta calidad Web
+Prototipo de alta calidad Web, puedes interactuar en el siguiente link: 
 https://www.figma.com/proto/FvJbW32xEfRt09FdlptMBF/Untitled?node-id=3%3A0&scaling=scale-down
-
-
-
 
 
 ***
 
 ## Test de Usabilidad
-  Realizamos testeo de usabilidad con el primer prototipo de alta fidelidad. Luego realizamos iteraciones.
- (PONER AQUÍ VIDEOS)
+ 
+Elegimos a distintos para realizar un test de usabilidad en cual podían darnos sus opiniones respecto a la aplicación. 
+
+* Usuario n°1 
+
+* Usuario n°2
+
+* Usuario n°3
 
 ***
 
 ## Iteraciones
 Nuestro primer prototipo de alta fidelidad era el siguiente:
+
 ![ Prototipo de alta fidelidad](/src/img/prototipoAFversion1.png)
 Luego de los testeos de usabilidad y una evaluación del diseño, redujimos la paleta de colores y pusimos un botón visible en el inicio.
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 
 import  POKEMON from './data/pokemon/pokemon.js';
 
-import {filtered} from './data.js'
+import {filtered} from './data.js';
 
-    //función de cambio de pantalla con el botón de ingresar
+//función de cambio de pantalla con el botón de ingresar
 document.getElementById('playBtn').addEventListener('click', () => {
     document.getElementById('screenTwo').style.visibility='visible';
     document.getElementById('screenTwo').style.display='block';
@@ -11,56 +11,57 @@ document.getElementById('playBtn').addEventListener('click', () => {
     document.getElementById('screenOne').style.display='none';
 
     //guardando div de html en variable
-    let container= document.getElementById('pokemon')
-    
+    let container= document.getElementById('pokemon');
+   
     //recorriendo arreglo
     for (let i = 0; i < POKEMON.length; i++) {
-
         //imprimiendo nombre e imagen de cada pokemon en Html
         container.innerHTML+=
         `<div class='cajitaPokemon'>
-        <div> <img class='imagen' src= "${POKEMON[i].img}"><div>
-        <div class='nombre'>   #${POKEMON[i].num}<div>
-        <div class='nombre'>    ${POKEMON[i].name}<div>
-        <input type='button' value= 'Ver +'>
+        <div> <img class='imagen' src='${POKEMON[i].img}'><div>
+        <h2> #${POKEMON[i].num}</h2>
+        <h2> ${POKEMON[i].name}</h2>
+        <h3> Caramelos: ${POKEMON[i].candy}
+        <br>
+         Mide: ${POKEMON[i].height}
+        <br>
+         Peso: ${POKEMON[i].weight}</h3>
+         
         </div>`
+
     }
 })
 
-    //agregando función de filtrado a la barra de selección de tipo de pokemon
+ //agregando función de filtrado a la barra de selección de tipo de pokemon
 document.getElementById('selectorType').addEventListener('change',()=>{
 
     //vaciando contenedor
    document.getElementById('pokemon').innerHTML="";
 
     //guardando el tipo de pokemon seleccionado
-    let filterType = document.getElementById('selectorType').value;
-  console.log(filterType);
+   let filterType = document.getElementById('selectorType').value;
+   console.log(filterType);
 
     // función de filtrado está en data.
 
     //guardando lista de pokemon filtrados en una variable
     let chosenType = (filtered (filterType));
-
+    function funcionconectada () {
+        return console.log('funciona')
+      };
     //recorriendo la lista de pokemon filtrados
     for (let i = 0; i < chosenType.length; i++) {
 
     //imprimiendo lista de pokemon en html
-    document.getElementById('pokemon').innerHTML += `
-    <div class='cajitaPokemon'>
-    <div> <img class='imagen' src= "${chosenType[i].img}"><div>
-    <div class='nombre'>    #${chosenType[i].num}<div>
-    <div class='nombre'>    ${chosenType[i].name}<div>  
-    <input type='button' value= 'Ver +'>
+    document.getElementById('pokemon').innerHTML += 
+    `<div class='cajitaPokemon'>
+    <div> <img class='imagen' src='${chosenType[i].img}'><div>
+     <h2> #${chosenType[i].num}</h2>
+     <h2> ${chosenType[i].name}</h2>
+     <h3> Caramelos: ${chosenType[i].candy}
+     <br>
+      Mide: ${chosenType[i].height}
+     <br>
+      Peso: ${chosenType[i].weight}</h3>
     </div>`
 }})
-
-/*//función abriendo ventana
-document.getElementById('')
-document.getElementById('pokemon').innerHTML = `
-<div class='cajitaPokemon'>
-    <div> <img class='imagen' src= "${chosenType[i].img}"><div>
-    <div class='pokemonCard'>    ${chosenType[i].name}<div>
-    </div>`
-    ver https://www.youtube.com/watch?v=RXTCrhos5w8
-    */
