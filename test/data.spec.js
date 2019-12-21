714442cd8dd57b5ac5df74fc3eb1ea8402683797
catalina-alzamora <alzamoracatalina@gmail.com>
2019-12-21 11:07:26 -0400

    arreglando responsive y test unitario

--- test/data.spec.js
+++ test/data.spec.js
@@ -27,7 +27,7 @@ describe('ordering', () => {
   });
 });
 
-//inventando un arreglo para testear la función
+// inventando un arreglo para testear la función
 const pokemonArray = [{
     id: 15,
     num: '015',
@@ -46,16 +46,19 @@ const pokemonArray = [{
     ],
   }
 ]
+// función de filtrar pokémon por tipo
 describe('filtered', () => {
   it('Debería retornar "Pidgey" para pokemonArray y "Normal"', () => {
     expect(filtered(pokemonArray,'Normal')).toMatchObject([{"id": 16, "num": '016', "name": "Pidgey", "type": ["Normal","Flying"]}])
   })
 });
-
+//función de ordenar pokémon por nombre
 describe('ordering', () => {
-  it('Debería retornar "Beedrill" para la posición 0 de pokemonArray usando ordering', () => {
-    const sorted = ordering(pokemonArray, pokemonArray);
-    expect(sorted[0].name).toBe('Beedrill');
+  it('Debería retornar "-1" al comparar "Beedrill y Pidgey" para pokemonArray usando ordering', () => {
+    expect(ordering(pokemonArray[0], pokemonArray[1])).toBe(-1);
+  })
+  it('Debería retornar "1" al comparar "Pidgey y Beedrill" para pokemonArray usando ordering', () => {
+    expect(ordering(pokemonArray[1], pokemonArray[0])).toBe(1);
   })
 });
 
